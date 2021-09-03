ab87b17085982d8213be0e1a4eb3f7d2d275acc8
rubensmk <rubenskishimoto@gmail.com>
2021-09-03 16:39:57 -0300

    feat: Responsiveness for 1440,1024,768 and 425 screens

--- src/components/Profile/ProfileImg/index.tsx
+++ src/components/Profile/ProfileImg/index.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import styles from './profileimage.module.scss';
 
 export function ProfileImg() {
     return (
         <div className={styles.imageContent}>
+            <img src="https://avatars.githubusercontent.com/u/52255226?v=4" alt="profile_image" className={styles.image} />
             <div className={styles.frame} />
-            <Image src="https://avatars.githubusercontent.com/u/52255226?v=4" width="234" height="244" alt="profile_image" className={styles.image} />
         </div>
     );
 }
--- src/components/SideInfo/SocialMedia.tsx
+++ src/components/SideInfo/SocialMedia.tsx
@@ -7,19 +7,19 @@ export function SocialMedia() {
         <aside className={styles.social}>
             <div className={styles.list}>
                 <a href="https://github.com/rubensmk" target="_blank" rel="noreferrer">
-                    <SiGithub size={32} />
+                    <SiGithub />
                     <p className="tooltip">github.com/rubensmk</p>
                 </a>
                 <a href="https://discord.com/" target="_blank" rel="noreferrer">
-                    <SiDiscord size={32} />
+                    <SiDiscord />
                     <p className="tooltip">rubens.mk#7870</p>
                 </a>
                 <a href="https://www.linkedin.com/in/rubens-kishimoto/" target="_blank" rel="noreferrer">
-                    <SiLinkedin size={32} />
+                    <SiLinkedin />
                     <p className="tooltip">rubens-kishimoto</p>
                 </a>
                 <a href="https://www.instagram.com/rubens.mkishimoto/" target="_blank" rel="noreferrer">
-                    <SiInstagram size={32} />
+                    <SiInstagram />
                     <p className="tooltip">@rubens.mkishimoto</p>
                 </a>
             </div>
