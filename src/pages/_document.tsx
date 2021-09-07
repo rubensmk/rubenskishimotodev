914361375356ca6035db1c8dfbfdf8255e5c026b
rubensmk <rubenskishimoto@gmail.com>
2021-09-07 17:54:07 -0300

    feat:Add metatags for social sharing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,18 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet" />
+
+                    <meta property="og:url" content="https://www.rubenskishimoto.dev/" />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:title"
+                        content="{rubenskishimoto.dev}"
+                    />
+                    <meta
+                        property="og:description"
+                        content="Site Portfólio"
+                    />
+                    <meta property="og:image" content={"https://i.ibb.co/7nGWq71/portfolio-1.png"} />
                 </Head>
                 <body>
                     <Main />
