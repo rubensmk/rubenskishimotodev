1ea6f1d13b29deffd94deca3ac76df7103b2b6c9
rubensmk <rubenskishimoto@gmail.com>
2021-10-13 17:20:59 -0300

    fix: Profile Description Text

--- src/components/Profile/ProfileDescription/index.tsx
+++ src/components/Profile/ProfileDescription/index.tsx
@@ -8,7 +8,7 @@ export function ProfileDescription() {
             <h2>Desenvolvedor Full-Stack</h2>
             <p>
                 Formado em Engenharia de Computação pelo Centro Universitário do Estado do Pará (CESUPA),
-                atuando atualmente como desenvolvedor front-end na <a>Luby Software</a> utilizando ferramentas
+                atuando atualmente como desenvolvedor front-end utilizando ferramentas
                 como <strong>ReactJS, NextJS e React Native.</strong> Conhecimentos de backend utilizando frameworks Node.js como <strong>Express</strong> e <strong>AdonisJS</strong>,
                 trabalhando em conjunto com integrações com bancos de dados e diferentes serviços.
             </p>
