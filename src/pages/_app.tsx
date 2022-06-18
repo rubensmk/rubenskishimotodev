3a5080a6a3e5e85f45f49e891ddf78c1d4b28b70
rubensmk <rubenskishimoto@gmail.com>
2022-06-17 22:55:04 -0300

    minor adjust

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import '../styles/global.scss';
 
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
